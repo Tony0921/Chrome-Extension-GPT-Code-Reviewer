a419c73a9c532e806fd77ea4d25a5a7e456c55dd
陳棨煜 <tony32059613@gmail.com>
2023-04-09 02:20:43 +0800

    1.上傳多檔案 2.三按鈕功能 3.介面美化

--- content.js
+++ content.js
@@ -1,31 +1,84 @@
 console.log("contents");
 
+var inputField = document.getElementsByTagName("textarea")[0];
+var resault = "";
+
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if( request.message != "" ) {
-            // console.log(request)
-            start(request);
+    function (message, sender, sendResponse) {
+        if (message.type === "manual") {
+            chooseFiles("manual");
+        }
+        if (message.type === "api") {
+            chooseFiles("api");
+        }
+        if (message.type === "paramenters") {
+            chooseFiles("paramenters");
         }
     }
 );
 
-function start(req){
-    // alert("started");
-    var prompt = "write a manual for this program ";
-    var resault = prompt + "\r\n" + req.message;
-    var inputField = document.getElementsByTagName("textarea")[0];
+async function chooseFiles(type) {
+    var fileInput = document.createElement("input");
+    fileInput.type = "file";
+    fileInput.multiple = true; // enable multiple file selection
+    fileInput.click();
+    fileInput.addEventListener("change", async function () {
+        const files = fileInput.files;
+        // console.log(files);
+        const fileContents = [];
+        for (const file of files) {
+            const fileName = file.name;
+            // console.log("File name:", fileName);
+            const contents = await readFile(file);
+            fileContents.push({ fileName, contents });
+            merge(contents, fileName);
+        }
+        console.log(fileContents);
+        console.log("finish!");
+        addPrompt(type);
+    });
+}
+
+function readFile(file) {
+    return new Promise((resolve) => {
+        const reader = new FileReader();
+        reader.readAsText(file);
+        reader.onload = () => resolve(reader.result);
+    });
+}
+
+function merge(content, fileName) {
+    var open = "read " + fileName + " as follows, summary in 10 words";
+    resault += open + "\r\n" + content + "\r\n";
+}
+
+function addPrompt(type) {
+    var prompt = "";
+    if (type == "manual") {
+        prompt = "write a manual for this program";
+    } else if (type == "api") {
+        prompt = "write the API list and description for this program with a markdown table";
+    } else if (type == "paramenters") {
+        prompt = "show all parameters, description and its location in this program as a markdown table";
+    }
+    resault += prompt;
+    console.log("resault:", resault);
+    setFieldValue();
+}
+
+function setFieldValue() {
     inputField.value = resault;
     inputField.defaultValue = resault;
 
+    // enable button
     var btn = document.getElementsByClassName("absolute") && document.getElementsByClassName("p-1")[0];
-    // console.log(btn);
     btn.disabled = false;
-    
-    if(inputField.scrollHeight < 200){
+
+    // set textfield height
+    if (inputField.scrollHeight < 200) {
         inputField.setAttribute("style", " max-height:200px; height:" + (inputField.scrollHeight) + "px; overflow-y: hidden;");
-    }else{
+    } else {
         inputField.setAttribute("style", "max-height:200px; height:" + (inputField.scrollHeight) + "px;");
     }
-    
+    resault = "";
 }
-
--- popup/popup.js
+++ popup/popup.js
@@ -1,33 +1,23 @@
 console.log("popup");
 
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("upload-btn").addEventListener("click", function () {
-        chooseFile();
+    document.getElementById("manual-btn").addEventListener("click", function () {
+        chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+            var activeTab = tabs[0];
+            chrome.tabs.sendMessage(activeTab.id, {type: "manual"});
+        });
     });
-    // popup("start"); //test
+    document.getElementById("api-btn").addEventListener("click", function () {
+        chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+            var activeTab = tabs[0];
+            chrome.tabs.sendMessage(activeTab.id, {type: "api"});
+        });
+    });
+    document.getElementById("paramenters-btn").addEventListener("click", function () {
+        chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+            var activeTab = tabs[0];
+            chrome.tabs.sendMessage(activeTab.id, {type: "paramenters"});
+        });
+    });
 });
 
-function chooseFile() {
-    var fileInput = document.createElement("input");
-    fileInput.type = "file";
-    fileInput.click();
-    fileInput.addEventListener("change", function () {
-        var file = fileInput.files[0];
-        var reader = new FileReader();
-        reader.onload = function () {
-            var contents = reader.result;
-            // Do something with the file contents
-            console.log(contents);
-            popup(contents);
-        };
-        reader.readAsText(file);
-    });
-}
-
-
-function popup(msg) {
-    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
-        var activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, {"message": msg});
-    });
-}
